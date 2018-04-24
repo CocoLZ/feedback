13b2bc635d9df7a0146d03fe35a9620fee832429
Yingjie Li <coco@Yingjies-MBP.fios-router.home>
2018-04-24 12:13:23 -0400

    tweak google strategy settings

--- services/passport.js
+++ services/passport.js
@@ -1,9 +1,9 @@
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("../config/keys");
-const mongoose = require("mongoose");
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
+const mongoose = require('mongoose');
 // model instance(fetch data from mongoose)
-const User = mongoose.model("users");
+const User = mongoose.model('users');
 
 passport.serializeUser((user, done) => {
   // token
@@ -21,7 +21,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
+      callbackURL: '/auth/google/callback',
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then(existingUser => {
