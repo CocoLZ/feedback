eb0cda256b1ba2e5e5ca2a687e33dcceee8877a1
Yingjie Li <coco@Yingjies-MBP.fios-router.home>
2018-05-29 14:56:36 -0400

    stripe token

---
+++ client/src/components/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Feedback</h1>
+      Collect feedback from your own users.
+    </div>
+  );
+};
+export default Landing;
